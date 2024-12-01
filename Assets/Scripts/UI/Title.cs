b1dc9df892b9fb767e186b572f02ad3264dd5fd3
alsgh990710 <alsgh990710@naver.com>
2024-12-01 17:50:30 +0900

    12.01

--- Assets/Scripts/UI/Title.cs
+++ Assets/Scripts/UI/Title.cs
@@ -2,20 +2,55 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
 public partial class Title : MonoBehaviour
 {
+    public JoyStick JOYSTICK;
+    
 
-    private void Start()
+    //private void Start()
+    //{
+    //    SetVideo();
+    //}
+
+    //public void OnBtnTitle()
+    //{
+    //    Shared.SceneMgr.ChangeScene(eSCENE.eSCENE_LOGIN);
+    //}
+
+    public void OnPointerDown(BaseEventData eventData)
     {
-        SetVideo();
+        JOYSTICK.gameObject.SetActive(true);
+
+
+
+#if UNITY_EDITOR_WIN
+#if UNITY_EDITOR
+        JOYSTICK.transform.position = Input.mousePosition;
+        
+    
+#else
+        Touch touch = Input.GetTouch(0);
+
+        JOYSTICK.transform.position = touch.position;
+#endif
+#endif
+        JOYSTICK.OnDown((PointerEventData)eventData);
     }
 
-    public void OnBtnTitle()
+    public void OnPointerUp(BaseEventData eventData)
     {
-        Shared.SceneMgr.ChangeScene(eSCENE.eSCENE_LOGIN);
+        JOYSTICK.gameObject.SetActive(false);
+        JOYSTICK.OnUp((PointerEventData)eventData);
+    }
+
+    public void OnDrag(BaseEventData eventData)
+    {
+        JOYSTICK.OnDrag((PointerEventData)eventData);
     }
 }
+
 
